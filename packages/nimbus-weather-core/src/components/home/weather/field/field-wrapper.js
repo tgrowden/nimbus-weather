204fe740f8f0b74001d9118c0bfce25ab9aa6dbb
Taylor Growden <taylorgrowden@gmail.com>
2018-07-09 20:32:07 -0400

    wip: increase performancy by lazy-loading components

---
+++ packages/nimbus-weather-core/src/components/home/weather/field/field-wrapper.js
@@ -0,0 +1,40 @@
+import * as React from 'react'
+import { withStyles } from '@material-ui/core/styles'
+import LazyLoad from 'react-lazyload'
+
+const styles = (theme: MuiTheme) => ({
+	root: {
+		borderBottom: `1px solid ${theme.palette.divider}`,
+		'&:not(:first-of-type)': {
+			paddingTop: theme.spacing.unit * 2
+		},
+		'&:not(:last-of-type)': {
+			paddingBottom: theme.spacing.unit * 2
+		},
+		'&:last-of-type': {
+			borderBottom: 'none'
+		}
+	}
+})
+
+type Props = {
+	classes: Object,
+	children: React.ReactNode
+}
+
+const FieldWrapper = ({
+	classes,
+	children,
+	...props
+}: Props) => {
+
+	return (
+		<LazyLoad>
+			<div className={classes.root} {...props}>
+				{children}
+			</div>
+		</LazyLoad>
+	)
+}
+
+export default withStyles(styles)(FieldWrapper)
